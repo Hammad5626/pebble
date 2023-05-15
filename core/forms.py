16545b950089fdb3d4ab9b35d1bf2d9cd8e8a71c
Hamad Badar <Hamadbadar5626@hotmail.com>
2023-05-15 11:20:15 +0500

    Format

--- core/forms.py
+++ core/forms.py
@@ -2,12 +2,13 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
+
 class LoginForm(AuthenticationForm):
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                'placeholder': 'Your username',
-                'class': 'w-full py-4 px-6 rounded-xl'
+                "placeholder": "Your username",
+                "class": "w-full py-4 px-6 rounded-xl",
             }
         )
     )
@@ -15,22 +16,23 @@ class LoginForm(AuthenticationForm):
     password = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                'placeholder': 'Your password',
-                'class': 'w-full py-4 px-6 rounded-xl'
+                "placeholder": "Your password",
+                "class": "w-full py-4 px-6 rounded-xl",
             }
         )
     )
 
+
 class SignupForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
-    
+        fields = ("username", "email", "password1", "password2")
+
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                'placeholder': 'Your username',
-                'class': 'w-full py-4 px-6 rounded-xl'
+                "placeholder": "Your username",
+                "class": "w-full py-4 px-6 rounded-xl",
             }
         )
     )
@@ -38,8 +40,8 @@ class SignupForm(UserCreationForm):
     email = forms.CharField(
         widget=forms.EmailInput(
             attrs={
-                'placeholder': 'Your email address',
-                'class': 'w-full py-4 px-6 rounded-xl'
+                "placeholder": "Your email address",
+                "class": "w-full py-4 px-6 rounded-xl",
             }
         )
     )
@@ -47,17 +49,17 @@ class SignupForm(UserCreationForm):
     password1 = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                'placeholder': 'Your password',
-                'class': 'w-full py-4 px-6 rounded-xl'
+                "placeholder": "Your password",
+                "class": "w-full py-4 px-6 rounded-xl",
             }
         )
     )
 
     password2 = forms.CharField(
         widget=forms.PasswordInput(
-        attrs={
-                'placeholder': 'Repeat password',
-                'class': 'w-full py-4 px-6 rounded-xl'
+            attrs={
+                "placeholder": "Repeat password",
+                "class": "w-full py-4 px-6 rounded-xl",
             }
         )
     )
--- core/urls.py
+++ core/urls.py
@@ -3,19 +3,20 @@ from django.urls import path
 from . import views
 from .forms import LoginForm
 
-app_name = 'core'
+app_name = "core"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('contact/', views.contact, name='contact'),
-    path('about/', views.about, name='about'),
-    path('privacy/', views.privacy, name='privacy'),
-    path('terms/', views.terms, name='terms'),
-    path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(
-            template_name='core/login.html',
-            authentication_form=LoginForm
+    path("", views.index, name="index"),
+    path("contact/", views.contact, name="contact"),
+    path("about/", views.about, name="about"),
+    path("privacy/", views.privacy, name="privacy"),
+    path("terms/", views.terms, name="terms"),
+    path("signup/", views.signup, name="signup"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="core/login.html", authentication_form=LoginForm
         ),
-        name='login'
+        name="login",
     ),
 ]
